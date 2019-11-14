34417c60db9459ef9a0247af09be6fc344f6f1a3
kittenbites <sammie.nagle@gmail.com>
2019-11-14 12:07:00 -0800

    implemented a getAllGrades method in app.jsx for database retrieval called after initial render in componentDidMount fetches data from the grades endpoint

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -9,6 +9,22 @@ class App extends React.Component {
     };
   }
 
+  componentDidMount() {
+    this.getAllGrades();
+  }
+
+  getAllGrades() {
+    fetch('/api/grades')
+      .then(value => value.json())
+      .then(grades => this.setState({ grades }))
+      .catch(error => console.error(error.message));
+  }
+
+  componentDidUpdate() {
+    // eslint-disable-next-line no-console
+    console.log(this.state.grades);
+  }
+
   render() {
     return <Header text="Student Grade Table"/>;
   }
