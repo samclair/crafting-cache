7c7376c8e2c4f825a74185a65e4d39e686771694
kittenbites <sammie.nagle@gmail.com>
2020-01-07 16:04:38 -0800

    Implemented backend api/grades to handle for addition, deletion, and fetching of student grades modified npm scripts to remove node JSON 'server' wrote handlers in grades.php for 'GET' 'POST' and 'DELETE' requests requests will now perform the appropriate action within the database

--- server/public/index.php
+++ server/public/index.php
@@ -3,6 +3,7 @@
 require_once '../api/_lifecycle.php';
 switch ($request['path']) {
   case '/api/health-check':
+  case'/api/grades':
     require_once "..${request['path']}.php";
   default:
     throw new ApiError("Cannot ${request['method']} ${request['path']}", 404);
