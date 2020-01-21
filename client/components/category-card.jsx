7d72fa83dc60d170a9f5b1680ad1f7c2014e4d53
kittenbites <sammie.nagle@gmail.com>
2020-01-21 15:27:21 -0800

    Added styling and better placeholders for category card:

--- client/components/category-card.jsx
+++ client/components/category-card.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function CategoryCard(props) {
   return (
-    <div className="card" style={{ width: '18rem', cursor: 'pointer' }}>
+    <div className="card mx-3 my-3" style={{ width: '18rem', cursor: 'pointer' }}>
       <div className="card-body">
         <h5 className="card-title">{props.categoryName}</h5>
         <h6 className="card-subtitle mb-2 text-muted">Inventory Count: </h6>
-        <p className="card-text">Example text</p>
+        <a href='#' className="card-text">View Inventory</a>
       </div>
     </div>);
 }
