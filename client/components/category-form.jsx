94c043764048df36f144ea37f1f509ff0bfbe404
kittenbites <sammie.nagle@gmail.com>
2020-01-21 15:15:05 -0800

    Repurposed grade form for category addition form

--- client/components/category-form.jsx
+++ client/components/category-form.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import FormInput from './forminput';
 
-class GradeForm extends React.Component {
+class CategoryForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: { input: '', isValid: false, isFocused: false },
-      course: { input: '', isValid: false, isFocused: false },
-      grade: { input: '', isValid: false, isFocused: false }
+      name: { input: '', isValid: false, isFocused: false }
     };
     this.textPattern = /^[A-Za-z \d]{3,64}$/;
     this.numberPattern = /^[\d]{1,2}$|^100$/;
@@ -19,20 +17,14 @@ class GradeForm extends React.Component {
 
   handleClear() {
     this.setState({
-      name: { input: '' },
-      course: { input: '' },
-      grade: { input: '' } });
+      name: { input: '' } });
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    if (this.state.name.input.match(this.textPattern) &&
-    this.state.course.input.match(this.textPattern) &&
-    this.state.grade.input.match(this.numberPattern)) {
+    if (this.state.name.input.match(this.textPattern)) {
       this.props.onSubmit({
-        name: this.state.name.input,
-        course: this.state.course.input,
-        grade: this.state.grade.input
+        name: this.state.name.input
       });
       this.handleClear();
     }
@@ -56,23 +48,9 @@ class GradeForm extends React.Component {
           <FormInput
             handleChange = {this.handleChange}
             handleBlur = {this.handleBlur}
-            symbol = "fa-user"
+            symbol = "fa-book"
             fieldName = "name"
             fieldValue = {this.state.name}
-          />
-          <FormInput
-            handleChange = {this.handleChange}
-            handleBlur = {this.handleBlur}
-            symbol = "fa-book"
-            fieldName = "course"
-            fieldValue = {this.state.course}
-          />
-          <FormInput
-            handleChange = {this.handleChange}
-            handleBlur = {this.handleBlur}
-            symbol = "fa-percent"
-            fieldName = "grade"
-            fieldValue = {this.state.grade}
           />
           <button type="submit" onClick={this.handleSubmit} className="btn btn-success mr-1">Add</button>
           <button type="button" onClick={this.handleClear} className="btn btn-secondary">Cancel</button>
@@ -82,4 +60,4 @@ class GradeForm extends React.Component {
   }
 }
 
-export default GradeForm;
+export default CategoryForm;
