1a186455f6a974a22f4cd301ba933f0afb4f8d01
kittenbites <sammie.nagle@gmail.com>
2020-01-22 15:18:04 -0800

    Implemented DELETE method for removing categories from database

--- server/api/categories.php
+++ server/api/categories.php
@@ -7,16 +7,38 @@ if ($request['method'] === 'GET') {
   send($response);
 }
 
+
+
 if ($request['method'] === 'POST') {
   $category = $request['body']['category'];
-  // if (!isset($category)) {
-  //   throw new ApiError("Missing category name", 400);
-  // }
+  if (!isset($category)) {
+    throw new ApiError("Missing category name", 400);
+  }
   add_category($link, $category);
   $response['body'] = [
     'categoryName' => $category,
   ];
   send($response);
+}
+
+if ($request['method'] === 'DELETE'){
+  $category = $request['body']['category'];
+  if (!isset($category)) {
+    throw new ApiError("Missing category name", 400);
+  }
+  delete_category($link, $category);
+  $response['body'] = [
+    'category' => 'deleted'
+  ];
+  send($response);
+}
+
+function delete_category($link, $category){
+  $sql = "
+  DELETE
+  FROM `categories`
+  WHERE `categories`.`categoryName`='$category'";
+  mysqli_query($link, $sql);
 }
 
 function get_all_categories($link)
@@ -29,9 +51,9 @@ function get_all_categories($link)
   return mysqli_fetch_all($result, MYSQLI_ASSOC);
 }
 
-function add_category($link, $name)
+function add_category($link, $category)
 {
   $sql = "INSERT INTO `categories` (`categoryId`, `categoryName`, `userId`)
-  VALUES (NULL, '$name', '1')";
+  VALUES (NULL, '$category', '1')";
   mysqli_query($link, $sql);
 }
