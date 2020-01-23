16aa384e9fa772c5492d050cc2ba2ee9d043ee67
kittenbites <sammie.nagle@gmail.com>
2020-01-23 12:08:14 -0800

    Updated form input for more versatility Form input now conditionally renders prepended symbol based on provided prop

---
+++ client/components/form-input.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function FormInput(props) {
+  let formIcon = props.symbol ? (<div className="input-group-prepend">
+    <div className="input-group-text">
+      <i className={'fas ' + props.symbol}></i>
+    </div>
+  </div>) : null;
+  return (
+    <div className="input-group mb-2">
+      {formIcon}
+      <input
+        onChange={props.handleChange}
+        onBlur = {props.handleBlur}
+        value={props.fieldValue.input}
+        name={props.fieldName}
+        className={'form-control ' + (props.fieldValue.isFocused ? (props.fieldValue.isValid ? 'is-valid' : 'is-invalid') : '')}
+        type="text"
+        placeholder={props.fieldName[0].toUpperCase() + props.fieldName.slice(1)} />
+    </div>
+  );
+}
+
+export default FormInput;
