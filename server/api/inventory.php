4b070d253e99899bc64d80c315b032ab93f0abd6
kittenbites <sammie.nagle@gmail.com>
2020-01-28 10:37:03 -0800

    Updated SQL request to join with tables to include units and categories

--- server/api/inventory.php
+++ server/api/inventory.php
@@ -7,16 +7,21 @@ if ($request['method'] === 'GET') {
   if(!isset($category)){
     throw new ApiError("Missing category from query param");
   }
-  $response['body'] = get_category_inventory($link);
+  $response['body'] = get_category_inventory($link,$category);
   send($response);
 }
 
-function get_category_inventory($link)
+function get_category_inventory($link,$category)
 {
   $sql = "
-  SELECT `itemName` as `name`, `itemId` as `id`, `unitId` as `amount`, `notes`
-  FROM `inventory`
-  WHERE `userId`=1";
+  SELECT `i`.`itemName` as `name`,
+  `i`.`itemId` as `id`,
+  CONCAT(`i`.`unitId`,' ',`u`.`unitName`) as `amount`,
+  `i`.`notes`
+  FROM `inventory` AS `i`
+  JOIN `units` AS `u` ON `u`.`unitId` = `i`.`unitId`
+  JOIN `categories` AS `c` ON `c`.`categoryId`=`i`.`categoryId`
+  WHERE `i`.`userId` = 1 AND `c`.`categoryName` = '{$category}'";
   $result = mysqli_query($link, $sql);
   return mysqli_fetch_all($result, MYSQLI_ASSOC);
 }
