4822f1658f5036dd3184d2ce117ea2bc5eb59744
kittenbites <sammie.nagle@gmail.com>
2020-01-27 17:04:53 -0800

    Added new inventory endpoint to accepted endpoints in index

--- server/public/index.php
+++ server/public/index.php
@@ -5,6 +5,7 @@ switch ($request['path']) {
   case '/api/health-check':
   case'/api/grades':
   case '/api/categories':
+  case '/api/inventory':
       require_once "..${request['path']}.php";
   default:
     throw new ApiError("Cannot ${request['method']} ${request['path']}", 404);
