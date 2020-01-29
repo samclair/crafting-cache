432a64b33ead74c1a278f4296d2af7ca8021d4d0
kittenbites <sammie.nagle@gmail.com>
2020-01-29 10:59:40 -0800

    Wrote formatUnits function to format unit strings in inventory table rows

--- client/components/inventory-row.jsx
+++ client/components/inventory-row.jsx
@@ -5,13 +5,21 @@ function InventoryRow(props) {
   return (
     <tr item={item.id}>
       <td scope='row'>{item.itemName}</td>
-      <td>{item.amount}</td>
+      <td>{formatUnits(item.amount)}</td>
       <td>{item.notes}</td>
       <td className="align-middle">
         Delete/Edit
       </td>
     </tr>
   );
+}
+
+function formatUnits(unitString) {
+  let unitComponents = unitString.split(' ');
+  if (unitComponents[0] > 1) {
+    unitComponents[1] = unitComponents[1] === 'inch' ? unitComponents[1] + 'es' : unitComponents[1] + 's';
+  }
+  return unitComponents.join(' ');
 }
 
 export default InventoryRow;
