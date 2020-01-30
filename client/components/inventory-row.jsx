92ff8a1bddbf1646d21d392ca829380d246f305a
kittenbites <sammie.nagle@gmail.com>
2020-01-30 09:15:47 -0800

    Converted inventory-row to a stateful class component in preparation for accepting updates

--- client/components/inventory-row.jsx
+++ client/components/inventory-row.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
 import Button from './button';
 
-function InventoryRow(props) {
-  const item = props.item;
-  return (
-    <tr item={item.id}>
-      <td scope='row'>{item.itemName}</td>
-      <td>{formatUnits(item.amount)}</td>
-      <td>{item.notes}</td>
-      <td className="align-middle">
-        <Button color='delete-button mb-auto align-self-left' symbol='fa-times' handleClick={() => props.handleDelete(item.id)} text='' />
-      </td>
-    </tr>
-  );
-}
-
-function formatUnits(unitString) {
-  let unitComponents = unitString.split(' ');
-  if (unitComponents[0] > 1) {
-    unitComponents[1] = unitComponents[1] === 'inch' ? unitComponents[1] + 'es' : unitComponents[1] + 's';
+class InventoryRow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: 'info'
+    };
   }
-  return unitComponents.join(' ');
+
+  formatUnits(unitString) {
+    let unitComponents = unitString.split(' ');
+    if (unitComponents[0] > 1) {
+      unitComponents[1] = unitComponents[1] === 'inch' ? unitComponents[1] + 'es' : unitComponents[1] + 's';
+    }
+    return unitComponents.join(' ');
+  }
+
+  render() {
+    const item = this.props.item;
+    return (
+      <tr item={item.id}>
+        <td scope='row'>{item.itemName}</td>
+        <td>{this.formatUnits(item.amount)}</td>
+        <td>{item.notes}</td>
+        <td className="align-middle">
+          <Button color='delete-button mb-auto align-self-left' symbol='fa-times' handleClick={() => this.props.handleDelete(item.id)} text='' />
+        </td>
+      </tr>
+    );
+  }
 }
 
 export default InventoryRow;
