927315b66ef68b5e888920d283fcd51cf9a1de6a
kittenbites <sammie.nagle@gmail.com>
2020-02-04 11:03:04 -0800

    Implemented user cookies to keep unique sessions

--- server/api/categories.php
+++ server/api/categories.php
@@ -2,8 +2,15 @@
 
 $link = get_db_link();
 
+if (!isset($_SESSION['user_id'])) {
+  $user_id = create_user($link);
+  $_SESSION['user_id'] = $user_id;
+} else {
+  $user_id = $_SESSION['user_id'];
+}
+
 if ($request['method'] === 'GET') {
-  $response['body'] = get_all_categories($link);
+  $response['body'] = get_all_categories($link, $user_id);
   send($response);
 }
 
@@ -12,7 +19,7 @@ if ($request['method'] === 'POST') {
   if (!isset($category)) {
     throw new ApiError("Missing category name", 400);
   }
-  $new_category_id  = add_category($link, $category);
+  $new_category_id  = add_category($link, $category, $user_id);
   $response['body'] = [
     'categoryName' => $category,
     'categoryId' => $new_category_id
@@ -57,23 +64,28 @@ function delete_category($link, $category_id){
   mysqli_query($link, $sql);
 }
 
-function get_all_categories($link)
-{
+function get_all_categories($link, $user_id){
   $sql = "
   SELECT `i`.`categoryId`, `c`.`categoryName`, COUNT(*) as `inventoryCount`
   FROM `inventory` AS `i`
   INNER JOIN `categories` AS `c`
   ON `i`.`categoryId` = `c`.`categoryId`
-  WHERE `i`.`userId` = 1
+  WHERE `i`.`userId` = $user_id
   GROUP BY `i`.`categoryId`";
   $result = mysqli_query($link, $sql);
   return mysqli_fetch_all($result, MYSQLI_ASSOC);
 }
 
-function add_category($link, $category)
-{
+function add_category($link, $category, $user_id){
   $sql = "INSERT INTO `categories` (`categoryId`, `categoryName`, `userId`)
-  VALUES (NULL, '$category', '1')";
+  VALUES (NULL, '$category', $user_id)";
   mysqli_query($link, $sql);
   return mysqli_insert_id($link);
+}
+
+function create_user($link){
+  $sql = "INSERT INTO `users` (`createdAt`) VALUES (CURRENT_TIMESTAMP)";
+  $link->query($sql);
+  $cartId = mysqli_insert_id($link);
+  return $cartId;
 }
