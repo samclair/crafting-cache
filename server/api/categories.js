d1f848034f3fd5e5a4d0a3fba7b0f060063edea7
kittenbites <sammie.nagle@gmail.com>
2020-02-23 10:54:51 -0800

    Requests now use async and await to prevent race conditions

--- server/api/categories.js
+++ server/api/categories.js
@@ -1,14 +1,15 @@
-let config = require('./_config.js');
+const config = require('./_config.js');
 const express = require('express');
-let mysql = require('mysql');
+const mysql = require('mysql');
+const util = require('util');
+
 const app = express();
 let connection = mysql.createConnection(config);
+const query = util.promisify(connection.query).bind(connection);
 
-app.listen(4000);
-connection.connect(err => { if (err) throw err; });
 app.get('/api/categories', getAllCategories);
 
-function getAllCategories(req, res) {
+async function getAllCategories(req, res) {
   const sql = `
   SELECT\`c\`.\`categoryName\`, \`c\`.\`categoryId\`, COUNT(\`itemName\`) as inventoryCount
   FROM\`categories\` AS\`c\`
@@ -16,11 +17,7 @@ function getAllCategories(req, res) {
   ON\`i\`.\`categoryId\` = \`c\`.\`categoryId\`
   WHERE\`c\`.\`userId\` = 1
   GROUP BY\`c\`.\`categoryId\``;
-  connection.query(sql, (err, results, fields) => {
-    if (err) {
-      throw err;
-    } else {
-      res.send(results);
-    }
-  });
+  const results = await query(sql);
+  res.send(results);
 }
+app.listen(4000);
