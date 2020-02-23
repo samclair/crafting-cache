686127008a8fd1a6a01b715f811d4ac8f7be5d01
kittenbites <sammie.nagle@gmail.com>
2020-02-22 17:19:04 -0800

    Implemented get request for categories endpoint in node

--- server/api/categories.js
+++ server/api/categories.js
@@ -1,11 +1,26 @@
-const config = require('./_config.js');
+let config = require('./_config.js');
 const express = require('express');
-const mysql = require('mysql');
+let mysql = require('mysql');
 const app = express();
 let connection = mysql.createConnection(config);
 
-// app.get('/api/categories');
-
-app.listen(3000);
-
+app.listen(4000);
 connection.connect(err => { if (err) throw err; });
+app.get('/api/categories', getAllCategories);
+
+function getAllCategories(req, res) {
+  const sql = `
+  SELECT\`c\`.\`categoryName\`, \`c\`.\`categoryId\`, COUNT(\`itemName\`) as inventoryCount
+  FROM\`categories\` AS\`c\`
+  LEFT JOIN\`inventory\` AS\`i\`
+  ON\`i\`.\`categoryId\` = \`c\`.\`categoryId\`
+  WHERE\`c\`.\`userId\` = 1
+  GROUP BY\`c\`.\`categoryId\``;
+  connection.query(sql, (err, results, fields) => {
+    if (err) {
+      throw err;
+    } else {
+      res.send(results);
+    }
+  });
+}
